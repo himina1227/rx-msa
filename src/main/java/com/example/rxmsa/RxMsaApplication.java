db7b50076b4bf76b09a1cc7bee7a09a2126fca08
KimNakgyeom <himina1227@gmail.com>
2022-10-30 01:10:20 +0900

    init commit

---
+++ src/main/java/com/example/rxmsa/RxMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rxmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rxmsa/RxMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rxmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
